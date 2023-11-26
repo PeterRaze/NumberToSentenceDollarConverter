9358019a7c83603b9ddcefd39abfd62122ed85a8
pedro.neto <pedromunizbneto@gmail.com>
2023-11-26 21:30:52 +0100

    Adding additional tests

--- NumberToCurrencyConverterTest/ConverterTest.cs
+++ NumberToCurrencyConverterTest/ConverterTest.cs
@@ -50,5 +50,64 @@ namespace NumberToCurrencyConverterTest
             Assert.Equal(expected, currency);
         }
 
+        [Theory]
+        [InlineData("0", "zero dollars")]
+        [InlineData("1", "one dollar")]
+        [InlineData("2", "two dollars")]
+        [InlineData("5", "five dollars")]
+        [InlineData("7", "seven dollars")]
+        public void Convert_One(string input, string expected)
+        {
+            string currency = Converter.ConvertNumberToSentece(input);
+            Assert.Equal(expected, currency);
+        }
+
+        [Theory]
+        [InlineData("10", "ten dollars")]
+        [InlineData("26", "twenty-six dollars")]
+        [InlineData("42", "forty-two dollars")]
+        [InlineData("83", "eighty-three dollars")]
+        [InlineData("91", "ninety-one dollars")]
+        public void Convert_Ten(string input, string expected)
+        {
+            string currency = Converter.ConvertNumberToSentece(input);
+            Assert.Equal(expected, currency);
+        }
+
+        [Theory]
+        [InlineData("200", "two hundred dollars")]
+        [InlineData("240", "two hundred forty dollars")]
+        [InlineData("413", "four hundred thirteen dollars")]
+        [InlineData("603", "six hundred three dollars")]
+        [InlineData("780", "seven hundred eighty dollars")]
+        public void Convert_Hundred(string input, string expected)
+        {
+            string currency = Converter.ConvertNumberToSentece(input);
+            Assert.Equal(expected, currency);
+        }
+
+        [Theory]
+        [InlineData("1 002", "one thousand two dollars")]
+        [InlineData("3 000", "three thousand dollars")]
+        [InlineData("4 150", "four thousand one hundred fifty dollars")]
+        [InlineData("42 309", "forty-two thousand three hundred nine dollars")]
+        [InlineData("732 015", "seven hundred thirty-two thousand fifteen dollars")]
+        public void Convert_Thousand(string input, string expected)
+        {
+            string currency = Converter.ConvertNumberToSentece(input);
+            Assert.Equal(expected, currency);
+        }
+
+        [Theory]
+        [InlineData("1 000 020", "one million twenty dollars")]
+        [InlineData("3 020 234", "three million twenty thousand two hundred thirty-four dollars")]
+        [InlineData("50 000 000", "fifty million dollars")]
+        [InlineData("66 301 233", "sixty-six million three hundred one thousand two hundred thirty-three dollars")]
+        [InlineData("932 020 305", "nine hundred thirty-two million twenty thousand three hundred five dollars")]
+        public void Convert_Million(string input, string expected)
+        {
+            string currency = Converter.ConvertNumberToSentece(input);
+            Assert.Equal(expected, currency);
+        }
     }
 }
